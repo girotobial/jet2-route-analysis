10dfcea82ac4af20679df2b98d384ea31d0738de
opensomersault <abrobinson1907@gmail.com>
2020-09-17 11:08:12 +0000

    add abstract base class and start Airport data scraper

--- src/data/scraper.py
+++ src/data/scraper.py
@@ -1,9 +1,23 @@
-import requests
+import abc
 import json
+from io import BytesIO
 from pathlib import Path
+from zipfile import ZipFile
+
+import requests
 
 
-class Jet2:
+class ABCScraper(abc.ABC):
+    @abc.abstractmethod
+    def download(self):
+        pass
+
+    @abc.abstractmethod
+    def save(self):
+        pass
+
+
+class Jet2(ABCScraper):
     def __init__(self):
         """Handles the download and saving of route data from the Jet2 API
         """
@@ -19,5 +33,23 @@ class Jet2:
 
     def save(self, file_path: Path):
         with open(file_path / 'jet2_data.json', 'w') as file:
-            json.dump(self.data['Data'], file)
+            json.dump(self.data, file)
         return self
+
+
+class AirportDatabase(ABCScraper):
+    def __init__(self):
+        self._zip_url = (
+            'https://www.partow.net/downloads/'
+            'GlobalAirportDatabase.zip'
+        )
+        self._column_url = (
+            'https://www.partow.net/miscellaneous/airportdatabase'
+            '/index.html#Downloads'
+        )
+
+    def download(self):
+        # TODO
+
+    def save(self):
+        # TODO
