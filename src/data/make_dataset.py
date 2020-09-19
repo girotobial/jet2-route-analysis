8541f809bf2c52fc4efb94307742ce371ce090fb
opensomersault <abrobinson1907@gmail.com>
2020-09-19 23:21:00 +0000

    add pre-processing step of dataset build

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,9 +1,78 @@
+# Default libraries
 import logging
 from pathlib import Path
 
+# Third party libraries
 import click
+import pandas as pd
 
+# Custom code
 import src.data.scraper as scraper
+
+
+def build_dataset(
+    jet2_data: pd.DataFrame,
+    airport_database: pd.DataFrame
+) -> pd.DataFrame:
+    """Takes raw datasets and constructs dataframe for analysis.
+
+    Parameters
+    ----------
+    jet2_data : pd.DataFrame
+        Raw Jet2 route data
+    airport_database : pd.DataFrame
+        Raw airport database data
+
+    Returns
+    -------
+    pd.DataFrame
+        Dataset for analysis
+    """
+    # Remove routes not available for booking or without destinations
+    jet2_data = jet2_data[
+        (jet2_data['isEnabledForBooking'] is True)
+        & (jet2_data['destinationIataCodes'] != '')
+    ]
+
+    # The dataset is focused on lat/long in decimal and standardised location
+    # names so the following columns are not needed.
+    jet2_data.drop(
+        columns=[
+            'searchTerms',
+            'airportUrlKey',
+            'isEnabledForBooking',
+            'country',
+            'label'
+        ],
+        inplace=True
+    )
+    airport_sub_titles = ['Degrees', 'Minutes', 'Seconds', 'Direction']
+    airport_database.drop(
+        columns=[
+            *[f'Latitude {sub}' for sub in airport_sub_titles],
+            *[f'Longitude {sub}' for sub in airport_sub_titles],
+            'ICAO Code',
+            'Airport Name',
+        ],
+        inplace=True
+    )
+    airport_database.rename(
+        columns={
+            'Latitude Decimal Degrees': 'Latitude',
+            'Longitude Decimal Degrees': 'Longitude'
+        },
+        inplace=True
+    )
+
+    jet2_data['destinationIataCodes'] = jet2_data['destinationIataCodes']\
+        .apply(lambda x: x.split('|'))
+    jet2_data = jet2_data.explode('destinationIataCodes')
+
+    # TODO Merge
+    
+    # FIXME
+    data = None
+    return data
 
 
 @click.command()
@@ -33,6 +102,15 @@ def main(input_filepath: str, output_filepath: str):
     logger.info('Scraping Global Aiport Database')
     airport_database = scraper.AirportDatabase()
     airport_database.download().save(input_filepath)
+
+    logger.info('Building dataset')
+    build_dataset(
+        jet2_data=jet2.dataframe,
+        airport_database=airport_database.dataframe
+    )
+
+    
+
 
 
 if __name__ == '__main__':
