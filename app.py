426435f7b97c93108e08fcd5234a095fd4d5abc8
opensomersault <abrobinson1907@gmail.com>
2020-09-28 17:24:51 +0100

    refactor: move data filtering to own function and add countries

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from typing import Dict
+
 import pandas as pd
 import streamlit as st
 
@@ -26,39 +28,70 @@ def filter_mask(
         return data[column].isin(values)
 
 
+def filter_data(data: pd.DataFrame, parameters: Dict[str, list]) -> pd.DataFrame:
+    """Selects rows based on passed parameters
+
+    Parameters
+    ----------
+    data : pd.DataFrame
+        Un Filtered dataset
+    parameters : Dict[list]
+        dictionary of lists with keys:
+        * destination_airport
+        * destination_country
+        * departure_airport
+        * departure_country
+
+    Returns
+    -------
+    pd.DataFrame
+        Filtered dataset
+    """
+    COLUMNS = dict(
+        destination_airport='Destination Airport Name',
+        destination_country='Destination Country',
+        departure_airport='Departure Airport Name',
+        departure_country='Departure Country',
+    )
+    mask = pd.Series([True] * len(data))
+    for param, column in COLUMNS.items():
+        mask = mask & filter_mask(data, column, parameters.get(param))
+
+    return data[mask].reset_index()
+
+
 def main():
     st.title('Jet2 Route Map')
     with st.spinner('Loading Data'):
         data = load_data()
 
-    departure_to_filter = st.sidebar.multiselect(
-        "Departure Airport",
-        data[
-            data['isDepartureAirport']
-        ]['Departure Airport Name'].unique().tolist()
-    )
+    departures = data[
+        data['isDepartureAirport']
+    ]['Departure Airport Name'].sort_values().unique().tolist()
+    destinations = data[
+        data['isDestinationAirport']
+    ]['Departure Airport Name'].sort_values().unique().tolist()
+    countries = data['Departure Country']\
+        .sort_values()\
+        .append(data['Destination Country'])\
+        .unique()
 
-    departure_mask = filter_mask(
-        data=data,
-        column='Departure Airport Name',
-        values=departure_to_filter
-    )
+    st.sidebar.title('Departure')
+    departure_airport = st.sidebar.multiselect('Departure Airport', departures)
+    departure_country = st.sidebar.multiselect('Departure Country', countries)
 
-    destination_to_filter = st.sidebar.multiselect(
-        "Destination Airport",
-        data[
-            data['isDestinationAirport']
-        ]['Departure Airport Name'].unique().tolist()
-    )
-    destination_mask = filter_mask(
-        data=data,
-        column='Destination Airport Name',
-        values=destination_to_filter
-    )
+    st.sidebar.title('Destination')
+    dest_airport = st.sidebar.multiselect('Destination Airport', destinations)
+    dest_country = st.sidebar.multiselect('Destination Country', countries)
 
-    filtered_data = data[
-        (departure_mask) & (destination_mask)
-    ].reset_index()
+    parameters = dict(
+        departure_airport=departure_airport,
+        destination_airport=dest_airport,
+        departure_country=departure_country,
+        destination_country=dest_country,
+    )
+    filtered_data = filter_data(data, parameters)
+
     st.plotly_chart(viz.plotly_route_map(filtered_data, title=None))
 
 
